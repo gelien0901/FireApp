6724b4ea0ed9612f41e7f7879714fb76a8a624c5
gellen0901 <201980292@psu.palawan.edu.ph>
2025-04-18 16:09:05 +0800

    files  for part1 fireapp

--- projectsite/projectsite/urls.py
+++ projectsite/projectsite/urls.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-
-from fire.views import HomePageView, ChartView
+from fire.views import HomePageView, ChartView, PieCountbySeverity, LineCountbyMonth, MultilineIncidentTop3Country, multipleBarbySeverity
 from fire import views
 
 urlpatterns = [
@@ -9,5 +8,9 @@ urlpatterns = [
     path('stations', views.map_station, name='map-station'),
     path('', HomePageView.as_view(), name='home'),
     path('dashboard_chart', ChartView.as_view(), name='dashboard-chart'),
+    path('chart/', PieCountbySeverity, name='chart'),
+    path('lineChart/', LineCountbyMonth, name='chart'),
+    path('multilineChart/', MultilineIncidentTop3Country, name='chart'),
+    path('multiBarChart/', multipleBarbySeverity, name='chart'),
 
 ]
